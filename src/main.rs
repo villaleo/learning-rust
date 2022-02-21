c189e9b9512e60ca606e75fae655a86ed14c5f6b
Leonardo Villalobos <levillalobos@csumb.edu>
2022-02-20 17:01:23 -0800

    Remove variables

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 mod guessing_game;
-mod variables;
 
 fn main() {
     // Invoke the function from guessing_game.rs
-    // guessing_game::main();
-
-    // Invoke the function from variables.rs
-    variables::sample();
+    guessing_game::main();
 }
